25e1321a6c329f4e0169e1af924c1d6e1eda8b10
Arnav Choudhury <66573183+8coolguy@users.noreply.github.com>
2024-06-09 14:21:37 -0700

    changed to upper case

--- game/firebase.py
+++ game/firebase.py
@@ -4,8 +4,8 @@ import pyrebase
 config={
   "apiKey": os.getenv("APIKEY"),
   "authDomain": os.getenv("AUTH"),
-  "databaseURL": os.getenv("databaseURL"),
-  "storageBucket": os.getenv("storageBucket")
+  "databaseURL": os.getenv("DB"),
+  "storageBucket": os.getenv("STORE")
 }
 #initialize firebase
 firebase = pyrebase.initialize_app(config)
