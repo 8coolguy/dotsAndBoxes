c40a19fa3501f004e4e1a9e8e33a731716e836c3
Arnav Choudhury <66573183+8coolguy@users.noreply.github.com>
2024-06-09 01:14:30 -0700

    settings math making

--- game/app.py
+++ game/app.py
@@ -19,13 +19,22 @@ socketio = SocketIO(app, cors_allowed_origins="*")
 # Handle Funciton Connection 
 @socketio.on("connect")
 def handleConnection():
-    emit("connect",{"r":DEFAULT//100 % 10,"c":DEFAULT//10 % 10,"n":2 % 10})
+    room_id = str(request.referrer.split("/")[-1])
+    if "?" in room_id: return
+    print(room_id)
+    setting = db.child("rooms").child(room_id).child("settings").get().val()
+    if not setting or setting == 0: setting = DEFAULT
+    print(setting)
+    emit("connect",{"r":setting//100 % 10,"c":setting//10 % 10,"n":setting % 10})
     print("User Connected")
 #
 # Handle Funciton Disconnect 
 @socketio.on("disconnect")
 def handleConnection():
     print("User Disconnected")
+    if session.get("uid",False):
+        db.child("players").child(session["uid"]).remove()
+        return
     room_id = str(request.referrer.split("/")[-1])
     if session.get("playerId",False):
         db.child("rooms").child(room_id).child("players").remove()
@@ -33,14 +42,15 @@ def handleConnection():
 # Handle Auto Id 
 @socketio.on("autoid")
 def handleConnection():
-    print("User Disconnected")
-    result = db.child("queue").push(1)
+    print("Id generated")
+    result = db.child("players").push(1)
+    session["uid"] = result["name"]
     join_room(result["name"])
 
 @socketio.on("init")
 def handleInit(rows,columns,numPlayers):
-    print(rows,columns,numPlayers)
     # join a room with all the players the id has to be unique to the room
+
     nextNum = 0
     room_id = str(request.referrer.split("/")[-1])
     players = db.child("rooms").child(room_id).child("players").get().val()
@@ -49,9 +59,7 @@ def handleInit(rows,columns,numPlayers):
         return emit("redirect","/")
     player_id = player_num = nextNum
     db.child("rooms").child(room_id).child("players").child(player_id).set(player_num)
-    
-    join_room(room_id)
-    
+    join_room(room_id)    
     session["playerId"] = player_num
     session["board"] = Board(rows,columns,numPlayers)
 
@@ -85,11 +93,36 @@ def getEdge(lineId,rows,cols):
 @socketio.on('move')
 def handleMove(lineId):
     player = session["playerId"]
-    print(player,session["board"].nextPlayer)
     # check whether the correct player moved
     if player != session["board"].nextPlayer: return
     room_id = str(request.referrer.split("/")[-1])
     emit("move",{"lineId":lineId,"color":["orange","purple","green","blue","yellow"][session["board"].nextPlayer]},to=room_id)
+
+#
+# Add to the queue
+#
+# Post to
+@socketio.on('queue')
+def pushQueue(settings):
+    #check queue to see if the correct count of players exist yet
+    queue = db.child("queue").get().val()
+    if not queue: return db.child("queue").child(session["uid"]).set(settings)
+    numPlayers = settings % 10
+    buffer = []
+    for player in queue.keys():
+        if queue[player] == 0 or queue[player] == settings and len(buffer) < numPlayers - 1:
+            if db.child("players").child(player).get().val():
+                buffer.append(player)
+            else: db.child("queue").child(player).remove()
+    if len(buffer) == numPlayers - 1:
+        room_id = db.child("rooms").push({"settings":settings})["name"]
+        for player in buffer:
+            emit("redirect","/game/"+room_id, to=player)
+            db.child("queue").remove(player)
+        emit("redirect","/game/"+room_id)
+    else:
+        db.child("queue").child(session["uid"]).set(settings)
+    
 #
 # Page Routes
 #
--- game/static/match.js
+++ game/static/match.js
@@ -1,9 +1,20 @@
 var socket;
 
 window.onload=connectSocket;
+window.addEventListener("beforeunload", function (e) {
+    var confirmationMessage = "\o/";
+    (e || window.event).returnValue = confirmationMessage;
+    socket.emit("disconnect");
+    return confirmationMessage;                            //Webkit, Safari, Chrome
+  });
 
 function connectSocket(){
     socket = io({autoconnect:false});
 	socket.emit("autoid");
-    socket.on("autoid",(res)=> console.log(res));
+    socket.on("redirect", (destination) => {window.location.href = destination;});
+}
+function findGame(){
+    let setting = 100*document.getElementById("r-select").value + 10*document.getElementById("c-select").value + 1*document.getElementById("n-select").value;
+    console.log(setting);
+    socket.emit("queue",setting);
 }
